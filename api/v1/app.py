8d1dc6b2950dd4369c729d96b93b4dac2e47abd7
johnconnor77 <jfcalleh@unal.edu.co>
2020-05-17 16:54:43 -0500

    changed environ by getenv method with default values

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,7 +2,7 @@
 """ Flask Application """
 from models import storage
 from api.v1.views import app_views
-from os import environ
+from os import getenv
 from flask import Flask, render_template, make_response, jsonify
 from flask_cors import CORS
 from flasgger import Swagger
@@ -40,10 +40,7 @@ Swagger(app)
 
 if __name__ == "__main__":
     """ Main Function """
-    host = environ.get('HBNB_API_HOST')
-    port = environ.get('HBNB_API_PORT')
-    if not host:
-        host = '0.0.0.0'
-    if not port:
-        port = '5000'
+    host = getenv('HBNB_API_HOST', default='0.0.0.0')
+    port = getenv('HBNB_API_PORT', default=5000)
+
     app.run(host=host, port=port, threaded=True)
