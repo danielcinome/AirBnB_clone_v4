bd8b3b271d41f25709a65a4ebe4b75649380b1cf
johnconnor77 <jfcalleh@unal.edu.co>
2020-05-17 18:20:43 -0500

    change where variables have being call before as PORT and HOST

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,9 +4,13 @@ from models import storage
 from api.v1.views import app_views
 from os import getenv
 from flask import Flask, render_template, make_response, jsonify
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
 from flasgger import Swagger
 from flasgger.utils import swag_from
+
+host = getenv('HBNB_API_HOST', '0.0.0.0')
+port = getenv('HBNB_API_PORT', 5000)
+
 
 app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
@@ -40,7 +44,5 @@ Swagger(app)
 
 if __name__ == "__main__":
     """ Main Function """
-    host = getenv('HBNB_API_HOST', default='0.0.0.0')
-    port = getenv('HBNB_API_PORT', default=5000)
 
     app.run(host=host, port=port, threaded=True)
