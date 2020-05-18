0e779686176802cc42da4a44db9f889a6dcfd1bc
johnconnor77 <jfcalleh@unal.edu.co>
2020-05-18 09:04:15 -0500

    Fixed how article was appended and class information was missing

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -2,9 +2,6 @@ $(document).ready(function () {
   const dictAmenities = {};
   let amenityNames = [];
   let outPut = '';
-  let article = document.createElement('article');
-  let place = {};
-  let i = 0;
 
   $('[type="checkbox"]').change(function () {
     if ($(this).is(':checked')) {
@@ -17,44 +14,35 @@ $(document).ready(function () {
     outPut = amenityNames.join(', ');
     $('.amenities h4').text(outPut);
   });
-  console.log('im here')
-  $.get('http://0.0.0.0:5001/api/v1/status/', (data, success) => {
-    if (success === 'success') {
+  $.get('http://0.0.0.0:5001/api/v1/status/', (data) => {
+    if (data.status === 'OK') {
       $('#api_status').addClass('available');
     }
-  }).fail((data, success) => {
-    if (success === 'error') {
-      $('#api_status').removeClass('available');
-    }
   });
+
   $.ajax({
-    type: "post",
-    url: "http://0.0.0.0:5001/api/v1/places_search",
-    data: "{}",
-    dataType: "JSON",
+    type: 'POST',
+    url: 'http://0.0.0.0:5001/api/v1/places_search',
+    data: '{}',
+    contentType: 'application/json',
     success: function (response) {
-      while (i < length(data)) {
-        place =  response[i];
-        article = `
-          <div class="title_box">
-            <h2>${place.name}</h2>
-            <div class="price_by_night">${place.price_by_night}</div>
-          </div>
-          <div class="information">
-            <div class="max_guest">${place.max_guest } Guest{% if place.max_guest != 1 %}s{% endif %}</div>
-            <div class="number_rooms">${place.number_rooms } Bedroom{% if place.number_rooms != 1 %}s{% endif %}</div>
-            <div class="number_bathrooms">${place.number_bathrooms } Bathroom{% if place.number_bathrooms != 1 %}s{% endif %}</div>
-          </div>
-          <div class="user">
-            <b>Owner:</b> ${place.user.first_name} ${place.user.last_name }
-          </div>
-          <div class="description">
-            ${place.description | safe }
-          </div>
-        `;
-        $('section.places').append(article);
-        i++;
+      for (let i = 0; i < response.length; i++) {
+        const place = response[i];
+        $('section.places').append('<article>' +
+                    '<div class="title_box">' +
+                    `<h2> ${place.name} </h2>` +
+                    `<div class="price_by_night"> $${place.price_by_night} </div>` +
+                    '</div>' +
+                        '<div class="information">' +
+                        `<div class="max_guest"> ${place.max_guest} Guest${place.max_guest !== 1 ? 's' : ''}</div>` +
+                        `<div class="number_rooms"> ${place.number_rooms} Bedroom${place.number_rooms !== 1 ? 's' : ''}</div>` +
+                        `<div class="max_guest"> ${place.number_bathrooms} Bathroom${place.number_bathrooms !== 1 ? 's' : ''}</div>` +
+                    '</div>' +
+                    '<div class = "description">' +
+                        `${place.description}` +
+                    '</div>' +
+                    '</article>');
       }
     }
   });
- });
+});
